0d3545f9e00403c4ae45ea796f3a6430e45556e7
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 08:49:49 -0300

    media.js

---
+++ media.js
@@ -0,0 +1,18 @@
+//fazer a media das notas 10, 6.5, 8, 7.5
+
+// let nota1 = 10
+// let nota2 = 6.5
+// let nota3 = 8
+// let nota4 = 7.5
+
+const notas = [10, 6.5, 8, 7.5]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) /notas.length
+
+const valorSorvete = [20.5, 12.5, 7.2, 9.8, 6.66]
+
+let mediaValorSorvete = (valorSorvete[0] + valorSorvete[1] + valorSorvete[2] + valorSorvete[3] + valorSorvete[4]) /mediaValorSorvete.length
+
+
+console.log(media)
+console.log(mediaValorSorvete)
