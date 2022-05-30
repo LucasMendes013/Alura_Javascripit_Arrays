2d2cd009eb8c3ff8877294911b579769e2523bdf
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 10:07:04 -0300

    Juntamos duas turmas de estudantes
    com a propriedade concat, funciona como concatenacao, precisamos no final criar uma variavel para armazenar essa valor concatenado

---
+++ juntandoturmas.js
@@ -0,0 +1,7 @@
+const salaPhyton = ['melissa', 'andre', 'arthur']
+
+const salaJavascript = ['juliana', 'leo', 'jussara']
+
+const salaConjunta = salaPhyton.concat(salaJavascript)
+
+console.log(salaConjunta)
