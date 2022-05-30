57beba93444dc4db4614e445098f45767c7aad5f
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 10:02:13 -0300

    A atualizacao de lista
    ocorre por meio do splice, onde o primeiro parametro é o numero do indice, o segundo é ate onde ira e o terceiro é para adicionar um novo indice sem prexisar usar o push

---
+++ atualizandoLista.js
@@ -0,0 +1,5 @@
+const listaDeChamada = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André']
+
+listaDeChamada.splice(1,2, 'Anderson')
+
+console.log(listaDeChamada)
