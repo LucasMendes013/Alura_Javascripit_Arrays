ad0ec0123249de5da61eb1a0e63a309cbbefcd76
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 10:19:45 -0300

    Lista de duas dimensoes
    Nada  mais, nada menos, que colocar um array dentro de outro array e depois aciona-lo via [][] duas vezes

---
+++ listaDeDuasDimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['Joao', 'ana', 'phara', 'gabriel']
+
+const mediasDeAlunos = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, mediasDeAlunos]
+
+console.log(`${listaDeNotasEAlunos[0][0]}, a sua media é ${listaDeNotasEAlunos[1][0]}`)
