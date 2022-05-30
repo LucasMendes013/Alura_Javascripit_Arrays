bee6b1d907baa29da62f60cafcd911bc8c113ac0
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-05-30 09:27:50 -0300

    Dividindo o número de alunos de uma sala
    Com o parâmetro slice(), podemos dividir o tamanho de indices de um Array, especificando onde irá começar a divisao e onde parará.

---
+++ dividindo.arrays.js
@@ -0,0 +1,6 @@
+const nomes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André']
+
+const sala1 = nomes.slice(0,nomes.length/2)
+const sala2 = nomes.slice(nomes.length/2)
+
+console.log(`Alunos da sala1 : ${sala1} e alunos da sala2: ${sala2}`)
